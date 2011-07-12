51f3d843c36f10c669f67d2cb37e0a1e9094a4eb
Rafael Xavier de Souza <rxaviers@gmail.com>
2011-07-11 22:49:09 -0300

    ContentHash created.

---
+++ content_hash/content_hash.js
@@ -0,0 +1,67 @@
+steal.plugins(
+  'jquery/controller')
+  .then(function() {
+
+$.Controller.extend('ContentHash',
+/* @static */
+{
+  /**
+   * Default options:
+   */
+  defaults: {
+    set_ev: 'set',                            // set content event
+    show_ev: 'show',                          // show content event
+    ready_ev: 'ready',                        // ready event
+    phaseOut: function(el) {
+        return el.hide();
+      },
+    phaseIn: function(el) {
+        return el.show();
+      }
+  }
+},
+/* @prototype */
+{
+  /**
+   *
+   */
+  init: function(el, options) {
+    this.content_els = {};
+    this.element
+    .bind(this.options.set_ev, this.callback('set'))
+    .bind(this.options.show_ev, this.callback('show'));
+    this.phaseIn = this.options.phaseIn;
+    this.phaseOut = this.options.phaseOut;
+  },
+
+  /**
+   *
+   */
+  set: function(ev, key, el) {
+    if(this.content_els[key]) {
+      this.content_els[key].remove();
+    }
+    this.content_els[key] = el;
+    this.element.append(el.hide());
+  },
+
+  /**
+   *
+   */
+  show: function(ev, key) {
+    this.phaseOut(this.element.children());
+    this.phaseIn(this.content_els[key]);
+    this.content_ready(key);
+  },
+
+  /**
+   *
+   */
+  content_ready: function(key) {
+    this.content_els[key]
+    .trigger(this.options.ready_ev);
+  }
+}
+);
+
+});
